45ff2877b32450676a34df70ced362818ab50f91
Nils Bergmann <nilsbergmann@noim.io>
2024-01-23 18:54:43 +0100

    refactor: useNow

--- reactive_home/src/composeables/useNow.ts
+++ reactive_home/src/composeables/useNow.ts
@@ -1,14 +1,24 @@
-import { ref } from "../dep.ts";
-import type { Ref } from "../dep.ts";
+import { shallowRef, tryOnScopeDispose } from "../dep.ts";
+import type { ShallowRef } from "../dep.ts";
 
 export type UseNowOptions = { interval?: number };
 
-export function useNow(options?: UseNowOptions) {
-  const value: Ref<Date> = ref(new Date());
+/**
+ * A Ref which updates with current Date by provided interval
+ * Default interval is 1000ms
+ * @param options
+ * @returns
+ */
+export function useNow(options?: UseNowOptions): ShallowRef<Date> {
+  const value = shallowRef(new Date());
 
-  setInterval(() => {
+  const interval = setInterval(() => {
     value.value = new Date();
-  }, options?.interval);
+  }, options?.interval ?? 1000);
+
+  tryOnScopeDispose(() => {
+    clearInterval(interval);
+  });
 
   return value;
 }
