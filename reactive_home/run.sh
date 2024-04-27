be18c5117b37acd328ad6e16fd1d9decc4438506
Nils Bergmann <nilsbergmann@noim.io>
2024-04-27 19:33:50 +0200

    fix: use fstab maybe

--- reactive_home/run.sh
+++ reactive_home/run.sh
@@ -22,9 +22,11 @@ mkdir -p /deno_cache/npm
 mkdir -p /deno_cache/deps
 mkdir -p /deno_cache/gen
 
-mergerfs /deno_image_cache/npm=RO:/config/reactive-home/deno_cache/npm /deno_cache/npm
-mergerfs /deno_image_cache/deps=RO:/config/reactive-home/deno_cache/deps /deno_cache/deps
-mergerfs /deno_image_cache/gen=RO:/config/reactive-home/deno_cache/gen /deno_cache/gen
+echo "/deno_image_cache/npm=RO:/config/reactive-home/deno_cache/npm   /deno_cache/npm   mergerfs    cache.files=off   0   0" >> /etc/fstab
+echo "/deno_image_cache/deps=RO:/config/reactive-home/deno_cache/deps   /deno_cache/deps   mergerfs    cache.files=off   0   0" >> /etc/fstab
+echo "/deno_image_cache/gen=RO:/config/reactive-home/deno_cache/gen   /deno_cache/gen   mergerfs    cache.files=off   0   0" >> /etc/fstab
+
+mount -a
 
 DENO_DIR=/deno_cache deno run --lock=/deno.lock --allow-read=/config/reactive-home/import_map.json,/config.yaml --allow-write=/config/reactive-home/import_map.json /update-import-map.ts
 DENO_DIR=/deno_cache deno run --lock=/deno.lock --allow-env --allow-net --allow-run --allow-sys --allow-read /run.ts --root /config/reactive-home
