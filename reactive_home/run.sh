46f8e4ea36348caf7ac1883b80a1d681f7549828
Nils Bergmann <nilsbergmann@noim.io>
2024-05-30 20:44:14 +0200

    fix: missing import map

--- reactive_home/run.sh
+++ reactive_home/run.sh
@@ -17,4 +17,4 @@ fi
 deno run --lock=/deno.lock --allow-read=/config/reactive-home/import_map.json,/config.yaml --allow-write=/config/reactive-home/import_map.json /update-import-map.ts
 
 echo "Load runtime..."
-echo "import 'reactive-home/runtime'" | deno run --allow-env --allow-net --allow-run --allow-sys --allow-read - --root /config/reactive-home
+echo "import 'reactive-home/runtime'" | deno run --import-map=/config/reactive-home/import_map.json --allow-env --allow-net --allow-run --allow-sys --allow-read - --root /config/reactive-home
